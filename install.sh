3897885e4f021062c28e29c5d4b139f3778ee28d
Martin Juhl <mj@casalogic.dk>
2011-05-11 08:57:44 +0200

    Small bugfix..

--- install.sh
+++ install.sh
@@ -143,6 +143,16 @@ if [ `lspci |grep VGA |wc -l` -eq 2 ]; then
    NVIDIABUSID=`echo "PCI:"\`lspci |grep VGA |grep nVidia |cut -f1 -d:\`":"\`lspci |grep VGA |grep nVidia |cut -f2 -d: |cut -f1 -d.\`":"\`lspci |grep VGA |grep nVidia |cut -f2 -d. |cut -f1 -d" "\``
 elif [ `lspci |grep 3D |wc -l` -eq 1 ]; then
    NVIDIABUSID=`echo "PCI:"\`lspci |grep 3D |grep nVidia |cut -f1 -d:\`":"\`lspci |grep 3D |grep nVidia |cut -f2 -d: |cut -f1 -d.\`":"\`lspci |grep 3D |grep nVidia |cut -f2 -d. |cut -f1 -d" "\`   
+else
+echo 
+echo "The BusID of the nVidia card can't be determined"
+echo "You must correct this manually in /etc/X11/xorg.conf.nvidia"
+echo "Please report this problem.."
+echo
+echo "Press Any Key to continue"
+echo
+read 
+
 fi
 
 
