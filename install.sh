194478594df9773e61b6a6eea3812bfb8a628aea
Martin Juhl <mj@casalogic.dk>
2011-05-12 23:51:09 +0200

    Bug fix for issue #14...

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ if [ $HOME = /root ]; then
     exit 2
 fi
 
-echo "Welcome to the bumblebee installation v.1.3.4"
+echo "Welcome to the bumblebee installation v.1.3.5"
 echo "Licensed under BEER-WARE License and GPL"
 echo
 echo "This will enable you to utilize both your Intel and nVidia card"
