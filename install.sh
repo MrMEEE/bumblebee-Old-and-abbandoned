cffd4a925bca3c118e12386b097b31e033029ae5
Martin Juhl <mj@casalogic.dk>
2011-05-19 22:34:09 +0200

    Uninstall Fedora Fixes provided by Paul Vriens..

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with bumblebee.  If not, see <http://www.gnu.org/licenses/>.
 #
-BUMBLEBEEVERSION=1.4.14
+BUMBLEBEEVERSION=1.4.15
 
 
 ROOT_UID=0
