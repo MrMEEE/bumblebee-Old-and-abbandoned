88e8702b124f46c59b1e8cddf2b9191b30d86c5b
Martin Juhl <mj@casalogic.dk>
2011-05-26 18:09:43 +0200

    Added info on power handling in the README file...

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with bumblebee.  If not, see <http://www.gnu.org/licenses/>.
 #
-BUMBLEBEEVERSION=1.5.15
+BUMBLEBEEVERSION=1.5.16
 
 #Determine Arch x86_64 or i686
 ARCH=`uname -m`
