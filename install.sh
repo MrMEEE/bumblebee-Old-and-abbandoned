4906ef6634cb6e3b8471e0631eb0697efbf1a969
Martin Juhl <mj@casalogic.dk>
2011-06-17 10:18:14 +0200

    fixed xorg.conf in the bugreport tool...

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with bumblebee.  If not, see <http://www.gnu.org/licenses/>.
 #
-BUMBLEBEEVERSION=1.6.41
+BUMBLEBEEVERSION=1.6.42
 
 #Determine Arch x86_64 or i686
 ARCH=`uname -m`
