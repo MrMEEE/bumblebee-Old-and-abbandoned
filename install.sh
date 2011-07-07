082ac9de0aedc2a8a773ff6075014b0b8d4bf526
Martin Juhl <mj@casalogic.dk>
2011-07-07 14:48:51 +0200

    Added bash completion for optirun..

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with bumblebee.  If not, see <http://www.gnu.org/licenses/>.
 #
-BUMBLEBEEVERSION=1.6.53
+BUMBLEBEEVERSION=1.6.54
 
 source stages/determinedistro
 
