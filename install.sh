a44926eb76e7c3c6917c0e082713d9c19a388a0d
Martin Juhl <mj@casalogic.dk>
2011-07-24 16:32:51 +0200

    Cosmetics to the init script...

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with bumblebee.  If not, see <http://www.gnu.org/licenses/>.
 #
-BUMBLEBEEVERSION=1.6.62
+BUMBLEBEEVERSION=1.6.63
 
 source stages/determinedistro
 
