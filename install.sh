f34fd66c7628f89d869d99f4de14a802db38be99
Martin Juhl <mj@casalogic.dk>
2011-07-24 15:06:57 +0200

    Fixed alot of issues around bumblebee-configuration Added support for manually selecting connected monitor in configuration Added sudo file from PPA to git...

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with bumblebee.  If not, see <http://www.gnu.org/licenses/>.
 #
-BUMBLEBEEVERSION=1.6.61
+BUMBLEBEEVERSION=1.6.62
 
 source stages/determinedistro
 
