6d750c13f054d4a82d172e1e5c3dbdd7826c197c
Martin Juhl <mj@casalogic.dk>
2011-07-28 14:03:40 +0200

    Merging changes from Lekensteyn into ecoptirun...
    Updating version number...

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with bumblebee.  If not, see <http://www.gnu.org/licenses/>.
 #
-BUMBLEBEEVERSION=1.6.71
+BUMBLEBEEVERSION=1.7.0
 
 source stages/determinedistro
 
