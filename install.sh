7d188ae6028a8d0a2a6522cff09dcd390f38a5e8
Martin Juhl <mj@casalogic.dk>
2011-07-29 23:24:08 +0200

    Bugfixes regarding checking for nVidia card status...

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with bumblebee.  If not, see <http://www.gnu.org/licenses/>.
 #
-BUMBLEBEEVERSION=1.7.6
+BUMBLEBEEVERSION=1.7.7
 
 source stages/determinedistro
 
