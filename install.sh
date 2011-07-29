d0624ed792dca2c52c0b342ac5fb16f871c4ab8a
Martin Juhl <mj@casalogic.dk>
2011-07-29 21:34:43 +0200

    Cleanup by Lekensteyn.. issue #516..

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with bumblebee.  If not, see <http://www.gnu.org/licenses/>.
 #
-BUMBLEBEEVERSION=1.7.5
+BUMBLEBEEVERSION=1.7.6
 
 source stages/determinedistro
 
